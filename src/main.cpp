36ef2fa6e7e50662cf3c65a01d8ff73bcc7151b8
Daniel Junior <daniel.souza.lima@outlook.com>
2023-10-10 14:37:54 -0300

    feat: created main with threads

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+
+#include "lcd/LCD.hpp"
+#include "sensor/humidity.hpp"
+
+// Tasks ID
+TaskHandle_t task1;
+TaskHandle_t task2;
+
+void setup() {
+  Serial.begin(115200);
+  HumiditySensor* sensor = new HumiditySensor(33);
+  LCDManager* lcd_manager = new LCDManager();
+
+  // __________________________________________________________________________
+  lcdTasks::taskPrintOnLCDParam_t* p1 = NULL;
+  p1 = (lcdTasks::taskPrintOnLCDParam_t*)malloc(
+      sizeof(lcdTasks::taskPrintOnLCDParam_t));
+
+  if (p1 == NULL) {
+    Serial.println("Error on malloc");
+    return;
+  }
+
+  p1->humiditySensor = sensor;
+  p1->lcdManager = lcd_manager;
+
+  xTaskCreatePinnedToCore(lcdTasks::taskPrintOnLCD, "Task-Print-On-LCD",
+                          configMINIMAL_STACK_SIZE + 2024, p1, 1, &task1,
+                          APP_CPU_NUM);
+
+  // __________________________________________________________________________
+  xTaskCreatePinnedToCore(
+      humuditySendorTasks::taskReadData, "Task-Read-Sensor-Data",
+      configMINIMAL_STACK_SIZE + 2024, sensor, 1, &task2, APP_CPU_NUM);
+}
+
+void loop() {}
