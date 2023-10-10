658eedbb6bd0ad523531377edaa7e8ad36691858
Daniel Junior <daniel.souza.lima@outlook.com>
2023-10-10 14:43:49 -0300

    docs: created docs for LCD class

--- src/lcd/LCD.cpp
+++ src/lcd/LCD.cpp
@@ -4,33 +4,33 @@
 #include "../sensor/humidity.hpp"
 
 LCDManager::LCDManager() {
-  this->__lcd = new LiquidCrystal_I2C(0x27, 20, 4);
-  this->__lcd->init();
-  this->__lcd->clear();
+  this->lcd_obj = new LiquidCrystal_I2C(0x27, 20, 4);
+  this->lcd_obj->init();
+  this->lcd_obj->clear();
 }
 
 LCDManager::~LCDManager() {
-  if (this->__lcd == NULL) {
+  if (this->lcd_obj == NULL) {
     return;
   }
-  delete this->__lcd;
-  this->__lcd = NULL;
+  delete this->lcd_obj;
+  this->lcd_obj = NULL;
 }
 
 LiquidCrystal_I2C* LCDManager::screenLCD() {
-  return this->__lcd;
+  return this->lcd_obj;
 }
 
 void LCDManager::Light(bool state) {
-  state ? this->__lcd->backlight() : this->__lcd->noBacklight();
+  state ? this->lcd_obj->backlight() : this->lcd_obj->noBacklight();
 }
 
 void LCDManager::printData(uint16_t data) {
-  this->__lcd->setCursor(0, 0);
-  this->__lcd->printf("raw: %4u (%.2f)", data, data / 4096.0);
+  this->lcd_obj->setCursor(0, 0);
+  this->lcd_obj->printf("raw: %4u (%.2f)", data, data / 4096.0);
 
-  this->__lcd->setCursor(0, 1);
-  this->__lcd->printf("voltage: %.1fv", data / 4096.0 * 3.3);
+  this->lcd_obj->setCursor(0, 1);
+  this->lcd_obj->printf("voltage: %.1fv", data / 4096.0 * 3.3);
 }
 
 namespace lcdTasks {
--- src/lcd/LCD.hpp
+++ src/lcd/LCD.hpp
@@ -3,26 +3,79 @@
 #include <LiquidCrystal_I2C.h>
 #include "../sensor/humidity.hpp"
 
+/**
+ * @class LCDManager
+ * @brief A class for managing an LCD display using the LiquidCrystal_I2C
+ * library.
+ *
+ * This class provides methods for initializing, controlling, and displaying
+ * data on an LCD screen.
+ */
 class LCDManager {
  public:
+  /**
+   * @brief Constructor for the LCDManager class.
+   */
   LCDManager();
+
+  /**
+   * @brief Destructor for the LCDManager class.
+   */
   ~LCDManager();
 
- public:
-  LiquidCrystal_I2C* __lcd;
+ private:
+  /**
+   * @brief Pointer to the LiquidCrystal_I2C object used for controlling the LCD
+   * screen.
+   */
+  LiquidCrystal_I2C* lcd_obj;
 
  public:
+  /**
+   * @brief Get a pointer to the LiquidCrystal_I2C object used for the LCD
+   * screen.
+   *
+   * @return A pointer to the LiquidCrystal_I2C object.
+   */
   LiquidCrystal_I2C* screenLCD();
+
+  /**
+   * @brief Print a 16-bit integer value on the LCD screen.
+   *
+   * @param data The integer value to be displayed on the LCD screen.
+   */
   void printData(uint16_t data);
+
+  /**
+   * @brief Control the backlight of the LCD screen.
+   *
+   * @param state If `true`, the backlight is turned on; if `false`, it is
+   * turned off.
+   */
   void Light(bool state);
 };
 
+/**
+ * @namespace lcdTasks
+ * @brief A namespace containing functions related to LCD tasks.
+ */
 namespace lcdTasks {
-// taskPrintOnLCD parameter
+/**
+ * @brief Struct representing the parameters for the taskPrintOnLCD function.
+ */
 typedef struct {
-  HumiditySensor* humiditySensor;
-  LCDManager* lcdManager;
+  HumiditySensor* humiditySensor;  // Pointer to a humidity sensor object.
+  LCDManager* lcdManager;          // Pointer to an LCDManager object.
 } taskPrintOnLCDParam_t;
 
+/**
+ * @brief Task function for printing humidity data on an LCD screen.
+ *
+ * This function is intended to be run as a task and prints humidity data on an
+ * LCD screen using the provided HumiditySensor and LCDManager objects.
+ *
+ * @param p A pointer to a taskPrintOnLCDParam_t struct containing the necessary
+ * parameters.
+ */
 void taskPrintOnLCD(void* p);
 }  // namespace lcdTasks
