b20c00d9691aaa23f56989ff852e024c7b9781a0
Daniel Junior <daniel.souza.lima@outlook.com>
2023-10-10 14:52:34 -0300

    docs: created docs for utils

--- src/utils/types.hpp
+++ src/utils/types.hpp
@@ -2,6 +2,18 @@
 
 #include <Arduino.h>
 
+/**
+ * @typedef byte
+ * @brief An alias for the unsigned 8-bit integer data type (uint8_t).
+ *
+ * This alias is often used to improve code readability and portability.
+ */
 typedef uint8_t byte;
 
+/**
+ * @typedef string_t
+ * @brief An alias for the C-style string (char*) data type.
+ *
+ * This alias is often used to represent strings in C++ code.
+ */
 typedef char* string_t;
--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -3,7 +3,22 @@
 #include <Arduino.h>
 #include "types.hpp"
 
+/**
+ * @namespace utils
+ * @brief A namespace containing utility functions.
+ */
 namespace utils {
+/**
+ * @brief Copy a block of memory from source to destination byte by byte.
+ *
+ * This function copies a block of memory from the source address to the
+ * destination address byte by byte. It is often used for low-level memory
+ * manipulation.
+ *
+ * @param dst Pointer to the destination memory location.
+ * @param src Pointer to the source memory location.
+ * @param size The number of bytes to copy.
+ */
 static inline void bytecpy(void* dst, const void* src, size_t size) {
   size_t i;
 
@@ -11,4 +26,4 @@ static inline void bytecpy(void* dst, const void* src, size_t size) {
     ((volatile byte*)dst)[i] = ((volatile const byte*)src)[i];
   }
 }
-}
+}  // namespace utils
