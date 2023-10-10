b86c7d1f1cde39519a43d9a82966c051fc58e150
Daniel Junior <daniel.souza.lima@outlook.com>
2023-10-10 14:47:27 -0300

    docs:  added docs for wifi class

--- src/network/wifi/wifi.cpp
+++ src/network/wifi/wifi.cpp
@@ -4,44 +4,44 @@
 
 #include "utils/utils.hpp"
 
-WifiManager* WifiManager::__instance = NULL;
+WifiManager* WifiManager::instance = NULL;
 
 char* WifiManager::ssid() {
-  return this->__ssid;
+  return this->wifi_ssid;
 }
 
 char* WifiManager::password() {
-  return this->__password;
+  return this->wifi_password;
 }
 
 void WifiManager::setSSID(const string_t ssid, uint8_t size) {
   if (size < MAX_SSID_LEN) {
-    utils::bytecpy(__ssid, ssid, size);
-    __ssid[size] = '\0';
+    utils::bytecpy(wifi_ssid, ssid, size);
+    wifi_ssid[size] = '\0';
     return;
   }
-  utils::bytecpy(__ssid, ssid, sizeof(__ssid));
-  __ssid[sizeof(__ssid) - 1] = '\0';
+  utils::bytecpy(wifi_ssid, ssid, sizeof(wifi_ssid));
+  wifi_ssid[sizeof(wifi_ssid) - 1] = '\0';
 }
 
 void WifiManager::setPassword(const string_t password, uint8_t size) {
   if (size < MAX_PASS_LEN) {
-    utils::bytecpy(__password, password, size);
-    __password[size] = '\0';
+    utils::bytecpy(wifi_password, password, size);
+    wifi_password[size] = '\0';
     return;
   }
-  utils::bytecpy(__password, password, sizeof(__password));
-  __password[sizeof(__password) - 1] = '\0';
+  utils::bytecpy(wifi_password, password, sizeof(wifi_password));
+  wifi_password[sizeof(wifi_password) - 1] = '\0';
 }
 
 WifiManager* WifiManager::getInstance() {
-  if (WifiManager::__instance == NULL) {
-    WifiManager::__instance = new WifiManager();
+  if (WifiManager::instance == NULL) {
+    WifiManager::instance = new WifiManager();
   }
-  return WifiManager::__instance;
+  return WifiManager::instance;
 }
 
 void WifiManager::runAccessPoint() {
   WiFi.mode(WIFI_AP);
-  WiFi.softAP(this->__ssid, this->__password);
+  WiFi.softAP(this->wifi_ssid, this->wifi_password);
 }
--- src/network/wifi/wifi.hpp
+++ src/network/wifi/wifi.hpp
@@ -3,32 +3,77 @@
 #include <Arduino.h>
 #include "../../utils/types.hpp"
 
+/**
+ * @brief Maximum length of the Wi-Fi SSID.
+ */
 #define MAX_SSID_LEN 32
+
+/**
+ * @brief Maximum length of the Wi-Fi password.
+ */
 #define MAX_PASS_LEN 32
 
+/**
+ * @class WifiManager
+ * @brief A class for managing Wi-Fi configuration and access point setup.
+ */
 class WifiManager {
  private:
-  static WifiManager* __instance;
-  char __ssid[MAX_SSID_LEN];
-  char __password[MAX_PASS_LEN];
+  /**
+   * @brief Private constructor to enforce singleton pattern.
+   */
+  WifiManager();
+
+  /**
+   * @brief Private instance variable for the singleton pattern.
+   */
+  static WifiManager* instance;
+
+  /**
+   * @brief Buffer for storing the Wi-Fi SSID.
+   */
+  char wifi_ssid[MAX_SSID_LEN];
+
+  /**
+   * @brief Buffer for storing the Wi-Fi password.
+   */
+  char wifi_password[MAX_PASS_LEN];
 
  public:
   /**
-   * @brief getter for __ssid.
-   * @return string_t wifi ssid 
+   * @brief Getter for the Wi-Fi SSID.
+   * @return A string containing the Wi-Fi SSID.
    */
   string_t ssid();
 
-
+  /**
+   * @brief Getter for the Wi-Fi password.
+   * @return A string containing the Wi-Fi password.
+   */
   string_t password();
 
+  /**
+   * @brief Set the Wi-Fi SSID.
+   * @param ssid A string containing the Wi-Fi SSID.
+   * @param size The size of the provided string (limited to MAX_SSID_LEN).
+   */
   void setSSID(const string_t ssid, uint8_t size);
 
+  /**
+   * @brief Set the Wi-Fi password.
+   * @param password A string containing the Wi-Fi password.
+   * @param size The size of the provided string (limited to MAX_PASS_LEN).
+   */
   void setPassword(const string_t password, uint8_t size);
 
+  /**
+   * @brief Get the singleton instance of the WifiManager class.
+   * @return A pointer to the WifiManager instance.
+   */
   static WifiManager* getInstance();
 
+  /**
+   * @brief Run an access point for Wi-Fi configuration.
+   */
   void runAccessPoint();
-
-  // Getter for password
 };
